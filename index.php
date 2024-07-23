ada3da063531606ace884835c08d052f3aa383a3
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-07-23 11:45:01 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>My Shop</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>List Of Clients</h2>
+        <a class="btn btn-primary" href="/myshop/create.php">New Client</a>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Created at</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                //Database credentials
+                $servername = 'localhost';
+                $username = 'root';
+                $password = '';
+                $database = 'myshop';
+
+                //Database connection
+                $connection = new mysqli($servername, $username, $password, $database);
+
+                if($connection->connect_error){
+                    die('Connection failed: ' . $connection->connect_error); // die is equivalent to exit
+                }
+                //Read data from database table
+                $sql = 'SELECT * FROM clients';
+                $result = $connection->query($sql);// execute query and store result
+
+                if(!$result){
+                    die('Query failed: ' . $connection->error);
+                }
+                //read each row
+                while($row = $result->fetch_assoc()){
+                    echo "
+                    <tr>
+                    <td>$row[id]</td>
+                    <td>$row[name]</td>
+                    <td>$row[email]</td>
+                    <td>$row[phone]</td>
+                    <td>$row[address]</td>
+                    <td>$row[created_at]</td>
+                    <td>
+                        <a class='btn btn-primary btn-sm' href='/myshop/edit.php?id=$row[id]'>Edit</a>
+                        <a class='btn btn-danger btn-sm' href='/myshop/delete.phpid=$row[id]'>Delete</a>
+                </tr>
+                    ";
+                }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
